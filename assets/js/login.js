2e4b16d26f277c81045b71b4ddcc4c94151c94f2
wgs66 <707205590@qq.com>
2022-06-09 16:46:09 +0800

    完成登录与注册

--- assets/js/login.js
+++ assets/js/login.js
@@ -71,7 +71,7 @@ $(function () {
         // 要把token存在本地
         localStorage.setItem('token', res.token);
         // 跳转到首页
-        // location.href = '/index.html';
+        location.href = '/index.html';
       },
     });
   });
